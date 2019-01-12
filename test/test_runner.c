2b72e81d0af185624c4b68e7ac6e78a98e8cf31f
Jamie Nuttall <jamie@wattu.com>
2019-01-12 09:12:35 +0000

    renamed store to model

--- test/test_runner.c
+++ test/test_runner.c
@@ -6,7 +6,7 @@
 #include "test_phev_register.c"
 #include "test_phev_pipe.c"
 #include "test_phev_service.c"
-#include "test_phev_store.c"
+#include "test_phev_model.c"
 
 
 int main()
@@ -113,13 +113,13 @@ int main()
 
 //  PHEV_STORE
 
-    RUN_TEST(test_phev_store_create_store);
-    RUN_TEST(test_phev_store_add_to_store);
-    RUN_TEST(test_phev_store_get_from_store);
-    RUN_TEST(test_phev_store_get_from_store_not_found);
-    RUN_TEST(test_phev_store_update_store);
-    RUN_TEST(test_phev_store_store_compare);
-    RUN_TEST(test_phev_store_compare_not_same);
-    RUN_TEST(test_phev_store_compare_not_set);
+    RUN_TEST(test_phev_model_create_model);
+    RUN_TEST(test_phev_model_set_register);
+    RUN_TEST(test_phev_model_get_register);
+    RUN_TEST(test_phev_model_get_register_not_found);
+    RUN_TEST(test_phev_model_update_register);
+    RUN_TEST(test_phev_model_register_compare);
+    RUN_TEST(test_phev_model_register_compare_not_same);
+    RUN_TEST(test_phev_model_compare_not_set);
     return UNITY_END();
 }
