74908cfd01be3dab23113080247832529857a5ed
Jamie Nuttall <jamie@wattu.com>
2020-01-27 20:39:34 +0000

    headlights xor

--- src/phev_service.c
+++ src/phev_service.c
@@ -536,7 +536,7 @@ message_t *phev_service_jsonInputTransformer(void *ctx, message_t *message)
         if (phevMessage)
         {
             message_t *out = phev_core_convertToMessage(phevMessage);
-
+            phev_core_XORMessage(out);
             if (out)
             {
                 return out;
