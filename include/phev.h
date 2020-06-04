96a490db2231942af7a00b1d4a691a2757d73dab
Jamie Nuttall <jamie@wattu.com>
2020-06-04 10:59:16 +0100

    added disconnect car header

--- include/phev.h
+++ include/phev.h
@@ -110,4 +110,5 @@ phevData_t * phev_getRegister(phevCtx_t * ctx, uint8_t reg);
 char * phev_statusAsJson(phevCtx_t * ctx);
 messagingClient_t * phev_createIncomingMessageClient(void);
 void phev_disconnect(phevCtx_t * ctx);
+void phev_disconnectCar(phevCtx_t * ctx);
 #endif
